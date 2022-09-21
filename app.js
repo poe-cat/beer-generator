91ac5c47b124f52500d8abf8d39af76f1a5cd3b9
Katarzyna Szewiel <84228264+poe-cat@users.noreply.github.com>
2022-09-21 12:10:08 +0200

    initial commit, random beer generator

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    fetch('https://api.punkapi.com/v2/beers/random')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        const name = data[0].name
+        const description = data[0].description
+        const {volume} = data[0]
+        const volumeValue = volume.value
+        const volumeUnit = volume.unit
+        console.log(volumeValue)
+        console.log(volumeUnit)
+    })
+})
